2ce6be213bb05ff17a9b4d6f954f24d04dfc754f
YukthaVarshikaJ <122212350+YukthaVarshikaJ@users.noreply.github.com>
2023-01-09 13:22:43 +0530

    Add files via upload

---
+++ easy 8.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,p=60,q=70,r=3,c=0;
+	scanf("%d%d%d",&p,&q,&r);
+	for(i=p;i<=q;i++)
+	{
+		if(r!=c)
+		{
+		printf("%d",i);
+		c++;
+}
+	else
+	c++;
+	continue;
+}
+}
+
