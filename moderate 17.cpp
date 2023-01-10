58253d5f7e44aa13a9543b54f1c7f05ca8c0c7eb
YukthaVarshikaJ <122212350+YukthaVarshikaJ@users.noreply.github.com>
2023-01-10 23:07:02 +0530

    Add files via upload

--- moderate 17.cpp
+++ moderate 17.cpp
@@ -1,12 +1,18 @@
-#include<stdio.h>
+include<stdio.h>
+void findFactorial(int,int *); 
 int main()
 {
-	int factorial,i,n;
-	int f=1;
-	printf("enter the number:");
-	scanf("%d",&n);
+ 	int i,factorial,n;
+ 	printf("Enter a number:");
+ 	scanf("%d",&n);
+ 	findFactorial(n,&factorial);
+ 	printf("Factorial of %d is: %d",n,factorial);
+ 	return 0;
+}
+void findFactorial(int n,int*factorial)
+{
+ 	int i;
+ 	*factorial=1;
 	for(i=1;i<=n;i++)
-	f=f*i;
-	printf("the factorial is=%d ",f);
-	
+ 	*factorial=*factorial*i;
 }
